4cd27b18239c12aa3332239a2feef586a7cf3da9
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2020-12-29 09:52:20 +0100

    Renamed/Refactored to generate_sundays.py, now supports dynamic columns from argv. Also added gitignore
    Signed-off-by: Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>

--- generate_sundays.py
+++ generate_sundays.py
@@ -1,9 +1,15 @@
 #!/usr/local/bin/python
+#
+# This script generates a csv file containing all the Sundays from now until
+# UNTIL_YEAR appending the arguments as extra columns
+#
 
+import csv
 import datetime
 import json
 import os
 import re
+import sys
 
 
 def lookup_req(param):
@@ -14,6 +20,9 @@ def lookup_req(param):
 
 
 FILE_DATETIME_FORMAT = '%Y%m%d%H%M%S%f'  # Format: '20190901120012345'
+UNTIL_YEAR = 2022
+EXTRA_COLUMNS = sys.argv[1:]
+OUTPUT_FILE = 'zakgeld.csv'
 
 try:
     LOG_LEVEL = os.environ['LOG_LEVEL']
@@ -21,7 +30,7 @@ except KeyError:
     LOG_LEVEL = 'INFO'
 
 
-def logger(level, src_key, msg):
+def logger(level, msg):
     log_levels = {
         'ERROR': 0,
         'WARNING': 1,
@@ -29,7 +38,7 @@ def logger(level, src_key, msg):
         'DEBUG': 3
     }
     if log_levels[LOG_LEVEL] >= log_levels[level]:
-        print(f"[{level}] [{src_key}] {msg}")
+        print(f"[{level}] {msg}")
 
 
 def validate_datetime(string, dt_format):
@@ -41,7 +50,7 @@ def validate_datetime(string, dt_format):
 
 
 def main():
-    until_year = 2021
+    until_year = UNTIL_YEAR
 
     d_delta = datetime.date(year=until_year, month=1, day=1) - datetime.date.today()
 
@@ -51,12 +60,17 @@ def main():
         if incr.weekday() == 6:
             output_list.append(incr.strftime("%A %d %B %Y"))
 
-    import csv
+    logger("INFO", f"Output file: {OUTPUT_FILE}")
+    with open(OUTPUT_FILE, mode='w') as target_file:
+        writer = csv.writer(target_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    with open('zakgeld.csv', mode='w') as zakgeld_file:
-        writer = csv.writer(zakgeld_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        ALL_COLUMNS = ['Date']
+        if EXTRA_COLUMNS:
+            ALL_COLUMNS = ['Date'] + EXTRA_COLUMNS
+        writer.writerow(ALL_COLUMNS)
 
-        writer.writerow(['Date', 'Alexa'])
+        logger("INFO", f"Columns: {ALL_COLUMNS}")
+
         for date in output_list:
             writer.writerow([date])
 
